6684d2f5b2b8e77c42cd7d4f1c7c1151d487a2e6
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2024-03-05 15:29:57 +1100

    added something to test the conversion of an input array to a state array

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-
+use aes::convert_vec_to_state_array;
 
 #[allow(dead_code, unused)]
+mod aes;
+mod constants;
+
 fn main(){
+    let test_vec: Vec<u8> = vec![
+        0x00,0x11,0x22,0x33,
+        0x44,0x55,0x66,0x77,
+        0x88,0x99,0xaa,0xbb,
+        0xcc,0xdd,0xee,0xff
+        ];
+    println!("{:02x?}",convert_vec_to_state_array(test_vec));
 }
 
 
