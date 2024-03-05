bd4293a234b33ea4fd873635bc6c7747cb36968c
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2024-03-06 10:40:28 +1100

    added a helper function to calculate the value by doing all of the XOR operations on a particular column in the state array

--- src/helper.rs
+++ src/helper.rs
@@ -1,3 +1,5 @@
+use crate::constants::{G_MUL_2, G_MUL_3, MIX_COL_MATRIX};
+
 pub fn decode_hex_string(s: &str) -> Vec<u8> {
     let mut ret:Vec<u8> = vec![];
     if s.len() % 2 != 0{
@@ -15,8 +17,55 @@ pub fn encode_hex_string(input:Vec<Vec<u8>>)->String{
     let mut string = "".to_string();
     for c in 0..input.len(){
         for r in 0..input[c].len(){
-            string += &format!("{:02x}",input[c][r]);
+            string += &format!("{:02x}",input[r][c]);
         }
     }
     string
 }   
+
+pub fn calc_mix_col_val(col:[u8;4], matrix_col_index:usize)->Option<u8>{
+    if matrix_col_index >3{
+        println!("matrix column index given to calc mix columns was incorrect, must be between 0 and 4");
+        return None;
+    }
+    let mix_col_multipliers = &MIX_COL_MATRIX[matrix_col_index*4..matrix_col_index*4+4];
+    let mut ret = 0u8;
+    for x in 0..4{
+        if mix_col_multipliers[x] == 1{
+            ret ^= col[x];
+        }
+        if mix_col_multipliers[x] == 2{
+            ret ^= G_MUL_2[col[x] as usize]
+        }
+        if mix_col_multipliers[x] == 3{
+            ret ^= G_MUL_3[col[x] as usize]
+        }
+    }
+
+    Some(ret)
+}
+#[allow(unused)]
+pub fn xor_vec(left:Vec<u8>, right:Vec<u8>)->Vec<u8>{
+    let mut ret: Vec<u8> = vec![];
+    let mut right = right.clone();
+    let mut left = left.clone();
+    if left.len() < right.len(){
+        //push the extra bits as they are 
+        for _ in 0..right.len()-left.len(){
+            ret.push(right.remove(0));
+        }
+        for x in 0..left.len(){
+            ret.push(left[x]^right[x]);
+        }
+    }if right.len() < left.len(){
+        for _ in 0..left.len()-right.len(){
+            ret.push(left.remove(0));
+        }
+        for x in 0..left.len(){
+            ret.push(left[x]^right[x]);
+        }
+    }
+    
+    
+    ret
+}
