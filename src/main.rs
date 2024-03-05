1a1c9c030d9b6ead7659e1e731800fcfae275023
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2024-03-05 16:33:05 +1100

    testing shift_rows functionality against NIST publication

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use aes::convert_vec_to_state_array;
+use aes::{convert_vec_to_state_array, shift_rows};
 
+use crate::helper::{decode_hex_string, encode_hex_string};
 #[allow(dead_code, unused)]
 mod aes;
 mod constants;
-
+mod helper;
 fn main(){
-    let test_vec: Vec<u8> = vec![
-        0x00,0x11,0x22,0x33,
-        0x44,0x55,0x66,0x77,
-        0x88,0x99,0xaa,0xbb,
-        0xcc,0xdd,0xee,0xff
-        ];
-    println!("{:02x?}",convert_vec_to_state_array(test_vec));
+    
+    let test_vec: Vec<u8> = decode_hex_string("63cab7040953d051cd60e0e7ba70e18c");
+    // println!("{:02x?}",test_vec);
+    let shifted = shift_rows(convert_vec_to_state_array(test_vec));
+    println!("{:02x?}",shifted);
+    //63 ca b7 04 
+    //09 53 d0 51
+    //cd 60 e0 e7
+    //ba 70 e1 8c
+    
+    //63 53 e0 8c 
+    //09 60 e1 04
+    //cd 70 b7 51
+    //ba ca d0 e7
+    
 }
 
 
