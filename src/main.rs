8affde765e9eb57117f6b8085146bbababadaaee
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2024-03-06 20:15:30 +1100

    main is now testing encrypt function

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ mod helper;
 fn main(){
     
     let test_vec: Vec<u8> = decode_hex_string("8ea2b7ca516745bfeafc49904b496089");
-    // println!("testv: {test_vec:02x?}");
     let key:Vec<u8> = decode_hex_string("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f");
     let x= decrypt(key, test_vec);
 }
