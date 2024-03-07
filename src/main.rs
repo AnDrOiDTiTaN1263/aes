f14aad106f1eb7e15ba29180ee8552f7be1bb695
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2024-03-07 15:07:57 +1100

    testing decrypt mode

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,10 @@ mod constants;
 mod helper;
 fn main(){
     
-    let test_vec: Vec<u8> = decode_hex_string("8ea2b7ca516745bfeafc49904b496089
-    ");
+    let test_vec: Vec<u8> = decode_hex_string("8ea2b7ca516745bfeafc49904b496089");
     // println!("testv: {test_vec:02x?}");
     let key:Vec<u8> = decode_hex_string("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f");
     let x= decrypt(key, test_vec);
-    
-    
 }
 
 
